94cec8fd3676f6d0cfec0cd068a175d2d1976a6a
Laurence Kohli <laurence.kohli@hes-so.ch>
2024-04-09 16:06:55 +0200

    TP - température cours 2

--- webmobui/src/App.vue
+++ webmobui/src/App.vue
@@ -1,30 +1,60 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+  import { computed, defineModel } from 'vue';
+  import AppScience from './components/AppScience.vue';
+
+  const tempSI = defineModel()
+  tempSI.value = 12;
+
+  function round(valeur){
+    return Math.round(valeur*100) / 100
+  }
+
+  //besoin de cela sinon on tourne en rond quand on appel set pour une autre car Kelvin changerait donc get appelé
+  const tempKelvin = computed({
+    get: () => {
+      return round(tempSI.value) ;
+    },
+    set: (value) => {
+      tempSI.value = (value);
+    }
+  });
+
+  // const tempCels = computed(() => {
+  //   return (tempKelvin.value - 273.15).toFixed(2) ;
+  // }); //affiche uniquement si Kelvin change => pas possible modif Celsius
+  //toFixed retourn un String !!
+
+  const tempCels = computed({
+    get: () => {
+    return round(tempSI.value - 273.15) ;
+    },
+    set: (value) => {
+      tempSI.value = (value + 273.15);
+    }
+  });
+
+  const tempFahr = computed({
+    get: () => {
+      return round((tempSI.value - 273.15)* 9/5 + 32) ;
+    },
+    set: (value) => {
+      tempSI.value = ((value - 32) * 5/9 + 273.15) 
+    }
+  });
+
+  console.log(tempCels.value)
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <AppScience :valeur ="tempKelvin" unite ="Kelvin"/> <br>
+  <input type="number" v-model="tempKelvin">
+  <input type="number" v-model="tempCels">
+  <input type="number" v-model="tempFahr">
+  <div v-if="tempKelvin < 0">
+    <p>La température est négative</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
---
+++ webmobui/src/components/AppScience.vue
@@ -0,0 +1,15 @@
+<script setup>
+  defineProps({
+    valeur : Number,
+    unite: String,
+})
+</script>
+
+<template>
+  <p>{{ unite }}</p>
+  <!-- <input type="number" v-model="valeur"> <br> -->
+</template>
+
+<style scoped>
+
+</style>
